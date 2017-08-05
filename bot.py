14732449b27eef3620833881f9d351e1a1ae07aa
Fawers <fabricioswerneck@gmail.com>
2017-08-05 18:30:56 -0300

    adapt bot to answer to both swedish and japanese verb queries

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os
+import re
 import configparser
 
 import telepot
@@ -12,50 +13,62 @@ config = configparser.ConfigParser()
 config.read(os.path.join(os.path.dirname(__file__), 'project.ini'))
 
 bot = telepot.Bot(config.get('bot', 'token'))
+reporter_id = config.get('bot', 'reporter_id')
 
 
-def _get_verb_info(verb):
-    try:
-        verb_info = verbix.sv.get_verb_info(verb)
-    except:
-        return 'Verbet `%s` hittades inte.' % verb
-
-    if verb_info is None:
-        return 'Verbet `%s` hittades inte.' % verb
-
-    def format(tuple):
-        v, f = tuple
-        if f == 'orto':
-            return '_%s_' % v
-        elif f == 'irregular':
-            return '*%s*' % v
-        else:
-            return v
-
-    infinitive = format(verb_info['forms']['infinitive'])
-    supine = format(verb_info['forms']['supine'])
-    gerund = format(verb_info['forms']['gerund'])
-    imperative = format(verb_info['forms']['imperative'])
-    present = format(verb_info['tenses']['present'])
-    past = format(verb_info['tenses']['past'])
-
-    url = verb_info['url']
-
-    return templates.swedish.template \
-        % (infinitive, supine, gerund, imperative, present, past) % url
+AVAILABLE_LANGUAGES = ['swedish', 'japanese']
+COMMAND_PATTERN = re.compile(
+    r'^/(%s)(?: (.*))?' % '|'.join(AVAILABLE_LANGUAGES))
 
 
 @misc.threaded
 def handle_message(msg):
-    verb = msg['text']
+    message = msg['text']
     chat_id = msg['chat']['id']
+    print(message)
+
+    match = COMMAND_PATTERN.match(message)
+
+    if not match:
+        return
 
     bot.sendChatAction(chat_id, 'typing')
 
-    info = _get_verb_info(verb)
+    language = match.group(1)
+    verb = match.group(2)
 
-    bot.sendMessage(chat_id, info, parse_mode='markdown',
-                    disable_web_page_preview=True)
+    if verb is None:
+        bot.sendMessage(
+            chat_id, 'Tell me the verb you want me to conjugate along with '
+            'the command!\nExample: `/swedish sjunga`', parse_mode='markdown')
+        return
+
+    try:
+        verb_data = getattr(verbix, language).conjugate(verb)
+        response = getattr(templates, language).render_response(verb_data)
+        bot.sendMessage(chat_id, response[0], parse_mode='markdown',
+                        reply_markup=response[1])
+
+    except verbix.VerbNotFoundError as vnf:
+        bot.sendMessage(chat_id, f'Couldn\'t find the verb _{vnf}_. :(',
+                        parse_mode='markdown')
+
+    except verbix.VerbixError as ve:
+        bot.sendMessage(
+            chat_id,
+            'Something happened while connecting to Verbix, and I could not '
+            'complete your request. Please try again while I send a report to '
+            'my developer.')
+        bot.sendMessage(reporter_id, f'Search for {language}:{verb}\n\n'
+                        f'{ve:r}.')
+
+    except Exception as e:
+        bot.sendMessage(
+            chat_id,
+            'Something strange happened while processing your request. '
+            'Please try again while I let my developer know about this.')
+        bot.sendMessage(
+            reporter_id, f'Exception raised with message {message}:\n\n{e:r}')
 
 
 def handle_inline(query):
